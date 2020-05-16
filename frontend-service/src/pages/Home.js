2505fc6313c67f9cca27b67fdd80c68858d9b1bf
collin lim <collinlim@collins-MacBook-Pro.local>
2020-05-17 02:54:20 +0800

    styled login and signup page

--- frontend-service/src/pages/Home.js
+++ frontend-service/src/pages/Home.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import CreateAccountCard from "../components/CreateAccountCard";
 import LoginCard from "../components/LoginCard";
-import { Grid } from "@material-ui/core";
+import "./Home.css";
 
 class Home extends Component {
   state = {
@@ -15,33 +15,20 @@ class Home extends Component {
   render() {
     return (
       <React.Fragment>
-        <Grid container direction="row" className="home">
-          <Grid item md={7} xs={12}></Grid>
-          <Grid
-            item
-            direction="row"
-            justify="center"
-            container
-            md={5}
-            xs={12}
-            className="home__card"
-          >
-            <Grid item>
-              {this.state.login ? (
-                <LoginCard
-                  handleChangePage={this.props.handleChangePage}
-                  handleChangeCard={this.handleChangeCard}
-                />
-              ) : (
-                <CreateAccountCard
-                  handleChangePage={this.props.handleChangePage}
-                  handleChangeCard={this.handleChangeCard}
-                  handleCreateUser={this.props.handleCreateUser}
-                />
-              )}
-            </Grid>
-          </Grid>
-        </Grid>
+        <div className="home">
+          {this.state.login ? (
+            <LoginCard
+              handleChangePage={this.props.handleChangePage}
+              handleChangeCard={this.handleChangeCard}
+            />
+          ) : (
+            <CreateAccountCard
+              handleChangePage={this.props.handleChangePage}
+              handleChangeCard={this.handleChangeCard}
+              handleCreateUser={this.props.handleCreateUser}
+            />
+          )}
+        </div>
       </React.Fragment>
     );
   }
