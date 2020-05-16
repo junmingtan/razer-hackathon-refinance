8d5409520910539fb77edfc786572f2dde468d00
stephen <ng.jiayu.stephen@gmail.com>
2020-05-16 17:00:35 +0800

    quests

--- frontend-service/src/components/Profile.jsx
+++ frontend-service/src/components/Profile.jsx
@@ -1,35 +1,5 @@
 import React, {Component, useState} from 'react';
-import './Profile.css';
-
-const ProgressBar = ({progress}) => (
-    <div className="progress">
-        <div className="main" style={{flexGrow: `${progress / 100}`}}>
-        </div>
-        <div style={{flexGrow: `${1 - (progress / 100)}`}}>
-            <div className="row" id="r-one">
-                <span className="sq" id="sq-1"/>
-                <span className="sq" id="sq-2"/>
-                <span className="sq" id="sq-3"/>
-            </div>
-            <div className="row" id="r-two">
-                <span className="sq" id="sq-4"></span>
-                <span className="sq" id="sq-5"></span>
-                <span className="sq" id="sq-6"></span>
-            </div>
-            <div className="row" id="r-three">
-                <span className="sq" id="sq-7"></span>
-                <span className="sq" id="sq-8"></span>
-                <span className="sq" id="sq-9"></span>
-            </div>
-            <div className="row" id="r-four">
-                <span className="sq" id="sq-10"></span>
-                <span className="sq" id="sq-11"></span>
-                <span className="sq" id="sq-12"></span>
-            </div>
-        </div>
-    </div>
-)
-
+import ProgressBar from "./ProgressBar";
 
 const Skill = ({name, icon}) => (
     <div className="skill">
@@ -39,12 +9,12 @@ const Skill = ({name, icon}) => (
 
 const Profile = ({level, experience, skills}) => {
     const [progress, setProgress] = useState(80);
-    return (<div className="profile_container">
+    return (<div className="container">
         <div className="level">
             Level {level} Grandmaster
         </div>
         <div className="experience_container" onClick={() => setProgress(Math.random() * 100)}>
-            <ProgressBar progress={progress}/>
+            <ProgressBar progress={progress} frontColor="#7BC415FF" backColor="#AEFF0088"/>
         </div>
         <div className="skills_container">
 
---
+++ frontend-service/src/components/ProgressBar.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import {pickHex} from "../utils"
+
+import "./ProgressBar.css"
+
+const ProgressBar = ({progress, frontColor, backColor}) => {
+    const fades = [0, 0.1, 0.3, 0.6, 0.7, 0.9, 1].map((w) => pickHex(backColor, frontColor, w));
+    return <ProgressBarInner progress={progress} fades={fades} />
+}
+
+const ProgressBarInner = ({progress, fades}) => {
+    return (
+    <div className="progress" style={{backgroundColor: fades[6]}}>
+        <div className="main" style={{backgroundColor: fades[0], flexGrow: `${progress / 100}`}}>
+        </div>
+        <div style={{flexGrow: `${1 - (progress / 100)}`}}>
+            <div className="row" id="r-one">
+                <span className="sq" style={{backgroundColor: fades[3]}}/>
+                <span className="sq" style={{backgroundColor: fades[5]}}/>
+                <span className="sq" style={{backgroundColor: fades[6]}}/>
+            </div>
+            <div className="row" id="r-two">
+                <span className="sq" style={{backgroundColor: fades[2]}}></span>
+                <span className="sq" style={{backgroundColor: fades[3]}}></span>
+                <span className="sq" style={{backgroundColor: fades[5]}}></span>
+            </div>
+            <div className="row" id="r-three">
+                <span className="sq" style={{backgroundColor: fades[1]}}></span>
+                <span className="sq" style={{backgroundColor: fades[2]}}></span>
+                <span className="sq" style={{backgroundColor: fades[3]}}></span>
+            </div>
+            <div className="row" id="r-four">
+                <span className="sq" style={{backgroundColor: fades[1]}}></span>
+                <span className="sq" style={{backgroundColor: fades[1]}}></span>
+                <span className="sq" style={{backgroundColor: fades[2]}}></span>
+            </div>
+        </div>
+    </div>
+)}
+
+export default ProgressBar;
