6a199665ab9b75dec2f5cec54b18f146069d5366
Tan Jun Ming <junming1996@hotmail.com>
2020-05-16 12:41:11 +0800

    debug

--- backend-service/src/application.py
+++ backend-service/src/application.py
@@ -19,4 +19,3 @@ if __name__ == "__main__":
     # Setting debug to True enables debug output. This line should be
     # removed before deploying a production app.
     application.run(host="0.0.0.0", threaded=True, debug=True)
-oytho
