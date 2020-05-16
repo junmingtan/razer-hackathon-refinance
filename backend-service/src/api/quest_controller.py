46fa762c3a1ec0312c05c0cc3b08ed2764688cc7
Tan Jun Ming <junming1996@hotmail.com>
2020-05-16 17:59:34 +0800

    quest and skilltree endpoints

--- backend-service/src/api/quest_controller.py
+++ backend-service/src/api/quest_controller.py
@@ -3,14 +3,26 @@ from db_driver.db_driver import db
 
 quest_api = Blueprint('quest_api', __name__)
 
-@quest_api.route('/quest/update', methods=['POST'])
-def createAccount():
-    user_id = request.json['user_id']
-    result = mambu_client.createCurrentAccount(user_id)
-    account_id = result['savingsAccount']['id']
-    return jsonify(account_id = account_id)
-
-@quest_api.route('/quest/<string:user_id>', methods=['GET'])
+@quest_api.route('/quest/<string:account_id>', methods=['GET'])
 def getAccount(account_id):
-    result = .getCurrentAccount(account_id)
+
+    result = None
+
+    ###
+    # Call database client here
+    ###
+
+    return jsonify(result)
+
+@quest_api.route('/quest/', methods=['POST'])
+def getAccount(account_id):
+    user_id = request.json['user_id']
+    skilltree = request.json['quest']
+
+    result = None
+
+    ###
+    # Call database client here
+    ###
+
     return jsonify(result)
--- backend-service/src/api/skilltree_controller.py
+++ backend-service/src/api/skilltree_controller.py
@@ -1,3 +1,28 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
+from db_driver.db_driver import get_skill_tree
 
 skilltree_api = Blueprint('skilltree_api', __name__)
+
+@skilltree_api.route('/skilltree/<string:account_id>', methods=['GET'])
+def getAccount(account_id):
+
+    result = None
+
+    ###
+    # Call database client here
+    ###
+
+    return jsonify(result)
+
+@skilltree_api.route('/skilltree/', methods=['POST'])
+def getAccount(account_id):
+    user_id = request.json['user_id']
+    skilltree = request.json['skilltree']
+
+    result = None
+
+    ###
+    # Call database client here
+    ###
+
+    return jsonify(result)
